a91dfce0f4d927e37703a13153f7c7836319344d
Erik Myhre <myherik@gmail.com>
2021-09-08 23:32:37 +0200

    Diverse valideringer og utseende endringer

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,10 +1,20 @@
 const skalHjem = () => {
   const check = $("#skalHjem").is(":checked");
   $("#hjemDato").attr("disabled", !check);
+  if (!check){
+    $("#hjemDato").val("");
+    $("#hjemDato").removeClass("is-valid");
+    $("#hjemDato").removeClass("is-invalid");
+  }
 }
 const skalBil = () => {
   const check = $("#regCheck").is(":checked");
   $("#regNummer").attr("disabled", !check);
+  if (!check){
+    $("#regNummer").val("");
+    $("#regNummer").removeClass("is-valid");
+    $("#regNummer").removeClass("is-invalid");
+  }
 }
 const videre = () => {
   const inputs = {
@@ -20,5 +30,50 @@ const videre = () => {
   }
   localStorage.setItem("formData", JSON.stringify(inputs))
   console.log(JSON.parse(localStorage.getItem("formData")));
+  
+}
+const validerUtreise = (item) => {
+  const date = item.value;
+  const today = new Date(Date.now()).getDay() + new Date(Date.now()).getMonth() + new Date(Date.now()).getFullYear();
+  const chosenDay = new Date(date).getDay() + new Date(date).getMonth() + new Date(date).getFullYear();
+  console.log(today);
+  console.log(chosenDay);
+  if (new Date(date) > new Date(Date.now()) || today === chosenDay){
+    item.classList.remove("is-invalid");
+    item.classList.add("is-valid");
+  }
+  else {
+    item.classList.remove("is-valid");
+    item.classList.add("is-invalid");
+  }
+}
+const validerHjemreise = (item) => {
+  const date = item.value;
+  const utreiseDate = $("#velgUtreise").val()
+  if(new Date(date) > new Date(utreiseDate)){
+    item.classList.remove("is-invalid");
+    item.classList.add("is-valid");
+  }
+  else if (item.value === ""){
+    item.classList.remove("is-valid");
+    item.classList.remove("is-invalid")
+  }
+  else {
+    item.classList.remove("is-valid");
+    item.classList.add("is-invalid");
+  }
+}
+const validerRegnummer = () => {
+  const regnummer = $("#regNummer").val()
+  const regexRegnummer = new RegExp(`[A-Z]{2}\\s[1-9]{1}[0-9]{4}`)
+  
+  if (regexRegnummer.test(regnummer)){
+    $("#regNummer").removeClass("is-invalid");
+    $("#regNummer").addClass("is-valid");
+  }
+  else {
+    $("#regNummer").removeClass("is-valid");
+    $("#regNummer").addClass("is-invalid");
+  }
   
 }
--- wwwroot/js/reisende.js
+++ wwwroot/js/reisende.js
@@ -17,11 +17,11 @@ const formatTable = () => {
         aria-controls="tableVoksen${i}">Fyll ut voksen #${i}</button><div class="collapse" id="tableVoksen${i}"><table class='table' >
         <tbody>
         <tr><td><label for="fornavnVoksen${i}">Fornavn</label></td>
-        <td><input type="text" id="fornavnVoksen${i}"></td></tr>
+        <td><input class="form-control" type="text" id="fornavnVoksen${i}"></td></tr>
         <tr><td><label for="etternavnVoksen${i}">Etternavn</label></td>
-        <td><input type="text" id="etternavnVoksen${i}"></td></tr>
+        <td><input class="form-control" type="text" id="etternavnVoksen${i}"></td></tr>
         <tr><td><label for="datoVoksen${i}">Fødselsdato</label></td>
-        <td><input type="date" id="datoVoksen${i}"></td></tr>
+        <td><input class="form-control" type="date" id="datoVoksen${i}"></td></tr>
         </tbody></table></div></td></tr>
       `
         $("#voksen").append(outVoksen)
@@ -34,14 +34,39 @@ const formatTable = () => {
         aria-controls="table${i}">Fyll ut barn #${i}</button><div class="collapse" id="table${i}"><table class='table' >
         <tbody>
         <tr><td><label for="fornavn${i}">Fornavn</label></td>
-        <td><input type="text" id="fornavn${i}"></td></tr>
+        <td><input onkeyup="validerFornavn(this)" class="form-control" type="text" id="fornavn${i}"></td></tr>
         <tr><td><label for="etternavn${i}">Etternavn</label></td>
-        <td><input type="text" id="etternavn${i}"></td></tr>
+        <td><input onkeyup="validerEtternavn(this)" class="form-control" type="text" id="etternavn${i}"></td></tr>
         <tr><td><label for="dato${i}">Fødselsdato</label></td>
-        <td><input type="date" id="dato${i}"></td></tr>
+        <td><input onchange="validerBursdag(this)" class="form-control" type="date" id="dato${i}"></td></tr>
         </tbody></table></div></td></tr>
       `
         $("#barn").append(outBarn)
     }
+}
+
+const validerFornavn = (item) => {
+    const navn = item.value;
+    const regNavn = new RegExp(`^([A-ZÆØÅ]{1}[a-zæøå]{0,}\\s{0,1}){1,}$`);
+    if (regNavn.test(navn)){
+        item.classList.remove("is-invalid");
+        item.classList.add("is-valid");
+    }
+    else {
+        item.classList.remove("is-valid");
+        item.classList.add("is-invalid");
+    }
     
+}
+const validerEtternavn = (item) => {
+    const navn = item.value;
+    const regNavn = new RegExp(`^[A-ZÆØÅ]{1}[a-zæøå]{0,}$`);
+    if (regNavn.test(navn)){
+        item.classList.remove("is-invalid");
+        item.classList.add("is-valid");
+    }
+    else {
+        item.classList.remove("is-valid");
+        item.classList.add("is-invalid");
+    }
 }
