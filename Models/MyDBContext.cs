e15f8e3a82b97d44bcd7f013cf248ab5a90341c6
Erik Myhre <myherik@gmail.com>
2021-09-10 21:26:18 +0200

    Added NuGets for backend and some DB code

---
+++ Models/MyDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gruppeoppgave1.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Kunde> Kunder { get; set; }
+        public DbSet<Bestilling> Bestillinger { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gruppeoppgave1.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,6 +19,7 @@ namespace Gruppeoppgave1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<MyDBContext>(options => options.UseSqlite("Data source=Bestilling.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
