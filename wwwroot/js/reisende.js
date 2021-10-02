94e63ec34daf03543099fca7c4a98c344665ffdd
s341833 <56063595+s341833@users.noreply.github.com>
2021-10-02 18:53:11 +0200

    minor bugfix

--- wwwroot/js/reisende.js
+++ wwwroot/js/reisende.js
@@ -8,7 +8,7 @@ $(()=>{
     }
     console.log(bestilling);
     setReisende();
-    formaterTable2();
+    formaterTable();
 })
 let reisendeVoksen = [];
 let reisendeBarn = [];
@@ -32,7 +32,7 @@ const setReisende = () => {
     }
 }
 
-const formaterTable2 = () => {
+const formaterTable = () => {
     const el = $("#reisende")
     let string = `<button onclick='setVoksen(0, this)' class='btn btn-outline-secondary'>Kontaktpersion 18+</button>`;
     for (let i = 1; i < Number(bestilling.reisende.voksne); i++) {
@@ -120,43 +120,6 @@ const setBarn = (index, item) => {
         $("#input-felter").attr('hidden', true)
         sjekkBestill();
     })
-}
-
-const formatTable = () => {
-    let outVoksen = ""
-    for (let i = 1; i <= Number(bestilling.reisende.voksne); i++) {
-        outVoksen = `<tr><td><button class="btn btn-outline-info" data-bs-toggle="collapse" 
-        data-bs-target="#tableVoksen${i}" 
-        aria-expanded="false" 
-        aria-controls="tableVoksen${i}">Fyll ut voksen #${i}</button><div class="collapse" id="tableVoksen${i}"><table class='table' >
-        <tbody>
-        <tr><td><label for="fornavnVoksen${i}">Fornavn</label></td>
-        <td><input onkeyup="validerFornavn(${i}, this, 'voksen')" class="form-control" type="text" id="fornavnVoksen${i}"></td></tr>
-        <tr><td><label for="etternavnVoksen${i}">Etternavn</label></td>
-        <td><input onkeyup="validerEtternavn(${i}, this, 'voksen')" class="form-control" type="text" id="etternavnVoksen${i}"></td></tr>
-        <tr><td><label for="datoVoksen${i}">Fødselsdato</label></td>
-        <td><input onchange="validerBursdag(${i}, this, 'voksen')" class="form-control" type="date" id="datoVoksen${i}"></td></tr>
-        </tbody></table></div></td></tr>
-      `
-        $("#voksen").append(outVoksen)
-    }
-    let outBarn = ""
-    for (let i = 1; i <= Number(bestilling.reisende.barn); i++) {
-        outBarn = `<tr><td><button class="btn btn-outline-info" data-bs-toggle="collapse" 
-        data-bs-target="#table${i}" 
-        aria-expanded="false" 
-        aria-controls="table${i}">Fyll ut barn #${i}</button><div class="collapse" id="table${i}"><table class='table' >
-        <tbody>
-        <tr><td><label for="fornavn${i}">Fornavn</label></td>
-        <td><input onkeyup="validerFornavn(${i}, this, 'barn')" class="form-control" type="text" id="fornavn${i}"></td></tr>
-        <tr><td><label for="etternavn${i}">Etternavn</label></td>
-        <td><input onkeyup="validerEtternavn(${i}, this, 'barn')" class="form-control" type="text" id="etternavn${i}"></td></tr>
-        <tr><td><label for="dato${i}">Fødselsdato</label></td>
-        <td><input onchange="validerBursdag(${i}, this, 'barn')" class="form-control" type="date" id="dato${i}"></td></tr>
-        </tbody></table></div></td></tr>
-      `
-        $("#barn").append(outBarn)
-    }
 }
 
 const validerFornavn = (index, item, type) => {
