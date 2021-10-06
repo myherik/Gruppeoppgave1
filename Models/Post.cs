1f701c3eae8c0d7aa15a8455dececa357b042e5f
s341833 <56063595+s341833@users.noreply.github.com>
2021-10-06 14:58:36 +0200

    added all postnummer and steder

--- Models/Post.cs
+++ Models/Post.cs
@@ -5,8 +5,10 @@ namespace Gruppeoppgave1.Models
     public class Post
     {
         [Key] 
-        public int PostNummer { get; set; }
+        [RegularExpression("^([0-9]{4})$")]
+        public string PostNummer { get; set; }
         [Required]
+        [RegularExpression("^[A-ZÆØÅ]{1}[a-zæøå]{0,}$")]
         public string PostSted { get; set; }
     }
 }
